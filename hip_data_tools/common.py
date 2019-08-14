a8d04ae52b0e9046e7e319f4825876152c2d967c
Arihant Surana <ari.surana@gmail.com>
2019-08-14 16:17:16 +1000

    feat: Added the ability to use git version for pypi releases (#2)
    * Added the ability to use git version for pypi releases
    
    * Added unit test for version counting method

--- hip_data_tools/common.py
+++ hip_data_tools/common.py
@@ -1,3 +1,10 @@
 import logging
+import os
 
 log = logging.getLogger(__name__)
+
+
+def get_release_version():
+    git_version = os.getenv("GIT_TAG", "v0.0")
+    pypi_version = git_version.lstrip("v").strip()
+    return pypi_version
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+from hip_data_tools.common import get_release_version
+
 setup(
     name='hip_data_tools',
     author='hipages Data Science Team',
     author_email='datascience@hipagesgroup.com.au',
-    version='1.0',
+    version=get_release_version(),
     description='Common utility functions for data engineering usecases',
     url='https://github.com/hipagesgroup/data-tools',
     packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
@@ -16,5 +18,5 @@ setup(
     tests_require=[
     ],
     python_requires='~=3.6',
-    license='osl-3.0'
+    license='osl-3.0',
 )
