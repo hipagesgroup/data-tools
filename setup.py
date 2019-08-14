ef0a9c9109c5624e7f99cb05b665d4114ea3d93d
Arihant Surana <ari.surana@gmail.com>
2019-08-14 15:55:10 +1000

    Added the ability to use git version for pypi releases

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+import os
+
 from setuptools import setup, find_packages
+
+git_version = os.getenv("GIT_TAG", "v0.0")
+pypi_version = git_version.lstrip("v")
 
 setup(
     name='hip_data_tools',
     author='hipages Data Science Team',
     author_email='datascience@hipagesgroup.com.au',
-    version='1.0',
+    version=pypi_version,
     description='Common utility functions for data engineering usecases',
     url='https://github.com/hipagesgroup/data-tools',
     packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
@@ -16,5 +21,5 @@ setup(
     tests_require=[
     ],
     python_requires='~=3.6',
-    license='osl-3.0'
+    license='osl-3.0',
 )
