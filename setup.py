c4f4d2f2c839a57dfee03ade3602c9e99a5eeb96
Arihant Surana <ari.surana@gmail.com>
2019-08-21 17:31:39 +1000

    chore: Read long description using readme file (#8)
    * Read long description using readme file
    
    * Read long description using readme file

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 """
 Setup and release this package for wider consumption using setup tools
 """
+import os
 
 from setuptools import setup, find_packages
 
+
+def get_long_description():
+    """
+    Get the contents of reame file as long_description
+    Returns: bytes containing readme file
+
+    """
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md'),
+              encoding='utf8') as readme_file:
+        return readme_file.read()
+
+
 setup(
-    long_description=open("README.md").read(),
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
     zip_safe=False,
     install_requires=[
