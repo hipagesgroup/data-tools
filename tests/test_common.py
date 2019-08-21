fbf5232a093e1c13afcac982b445fc2b0feffcce
Arihant Surana <ari.surana@gmail.com>
2019-08-22 09:39:00 +1000

    fix: Return to custom version handling as pbr does not work with semantic bot (#9)
    * fix: Return to custom version handling as pbr does not work with semantic bot
    
    * Updated test to check for non default case
    
    Co-Authored-By: Jake Warner <40750503+AmericanJake@users.noreply.github.com>
    
    * fix typo
    
    Co-Authored-By: Jake Warner <40750503+AmericanJake@users.noreply.github.com>
    
    * Updated test to check for non default case
    
    Co-Authored-By: Jake Warner <40750503+AmericanJake@users.noreply.github.com>

---
+++ tests/test_common.py
@@ -0,0 +1,23 @@
+import os
+from unittest import TestCase
+
+from hip_data_tools.common import get_release_version, get_long_description
+
+
+class TestCommon(TestCase):
+    def test__get_release_version__parses_tag(self):
+        os.environ["GIT_TAG"] = "v1.3"
+        actual = get_release_version()
+        expected = "1.3"
+        self.assertEquals(actual, expected)
+
+    def test__get_release_version__works_without_tag(self):
+        del os.environ["GIT_TAG"]
+        actual = get_release_version()
+        expected = "0.0"
+        self.assertEquals(actual, expected)
+
+    def test__get_long_description__reads_readme(self):
+        actual = get_long_description()[0:16]
+        expected = "# hip-data-tools"
+        self.assertEquals(actual, expected)
