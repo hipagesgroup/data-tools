53fe694bd2efb7317ec56f57c7a510bbcdc88420
Arihant Surana <ari.surana@gmail.com>
2019-08-21 14:56:09 +1000

    chore: refactored to use pbr in setuptools (#4)
    * feat: refactored to use pbr in setuptools
    
    * remove tests for version tracker as it is deprecated
    
    * remove all uses of get_release_version

--- setup.py
+++ setup.py
@@ -4,15 +4,7 @@ Setup and release this package for wider consumption using setup tools
 
 from setuptools import setup, find_packages
 
-from hip_data_tools.common import get_release_version
-
 setup(
-    name='hip_data_tools',
-    author='hipages Data Science Team',
-    author_email='datascience@hipagesgroup.com.au',
-    version=get_release_version(),
-    description='Common utility functions for data engineering usecases',
-    url='https://github.com/hipagesgroup/data-tools',
     packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
     zip_safe=False,
     install_requires=[
@@ -22,5 +14,5 @@ setup(
     tests_require=[
     ],
     python_requires='~=3.6',
-    license='osl-3.0',
+    pbr=True
 )
