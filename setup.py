e58734c508daa334cc4df99930f50af82a48bfe1
Arihant Surana <ari.surana@gmail.com>
2019-08-21 13:50:15 +1000

    feat: refactored to use pbr in setuptools

--- setup.py
+++ setup.py
@@ -7,12 +7,6 @@ from setuptools import setup, find_packages
 from hip_data_tools.common import get_release_version
 
 setup(
-    name='hip_data_tools',
-    author='hipages Data Science Team',
-    author_email='datascience@hipagesgroup.com.au',
-    version=get_release_version(),
-    description='Common utility functions for data engineering usecases',
-    url='https://github.com/hipagesgroup/data-tools',
     packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
     zip_safe=False,
     install_requires=[
@@ -22,5 +16,5 @@ setup(
     tests_require=[
     ],
     python_requires='~=3.6',
-    license='osl-3.0',
+    pbr=True
 )
