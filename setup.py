ae895a37bd3e09a49bbbac083ac27465ce41554a
Arihant Surana <ari.surana@gmail.com>
2019-08-22 09:04:09 +1000

    fix: Return to custom version handling as pbr does not work with semantic bot

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 """
 Setup and release this package for wider consumption using setup tools
 """
-import os
 
 from setuptools import setup, find_packages
 
-
-def get_long_description():
-    """
-    Get the contents of reame file as long_description
-    Returns: bytes containing readme file
-
-    """
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md'),
-              encoding='utf8') as readme_file:
-        return readme_file.read()
-
+from hip_data_tools.common import get_release_version, get_long_description
 
 setup(
     long_description=get_long_description(),
@@ -29,5 +18,5 @@ setup(
     tests_require=[
     ],
     python_requires='~=3.6',
-    pbr=True
+    version=get_release_version(),
 )
