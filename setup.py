2f9db5b790c58bcb40439215f1a567438964dd67
Arihant Surana <ari.surana@gmail.com>
2019-09-18 10:55:09 +1000

    Add dataframe methods to s3

--- setup.py
+++ setup.py
@@ -14,10 +14,13 @@ setup(
     install_requires=[
         "boto3==1.9.216",
         "joblib==0.13.2",
+        "pandas==0.25.1",
     ],
     test_suite="tests",
     tests_require=[
         "moto==1.3.13",
+        "pyarrow==0.14.1",
+        "python-snappy==0.5.4",
     ],
     python_requires='~=3.6',
     version=get_release_version(),
