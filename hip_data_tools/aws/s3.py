a9de0d57b0248d9e6a18ed5a55ff193d7b83b3be
Arihant Surana <ari.surana@gmail.com>
2019-09-18 10:55:09 +1000

    Add dataframe methods to s3

--- hip_data_tools/aws/s3.py
+++ hip_data_tools/aws/s3.py
@@ -1,7 +1,7 @@
 """
 Utility to connect to, and interact with the s3 file storage system
 """
-
+import pandas as pd
 import uuid
 
 from joblib import load, dump
@@ -57,18 +57,22 @@ class S3Util:
         self.download_file(s3_key=s3_key, local_file_path=local_file_path)
         return load(local_file_path)
 
-    def serialise_and_upload_object(self, obj, path_on_s3):
+    def serialise_and_upload_object(self, obj, s3_key):
         """
         Serialise any object to disk, and then upload to S3
         Args:
             obj (object): Any serialisable object
-            path_on_s3 (string): The absolute path on s3 to upload the file to
+            s3_key (string): The absolute path on s3 to upload the file to
         Returns: None
         """
 
-        random_tmp_file_nm = "/tmp/tmp_file" + str(uuid.uuid4())
+        random_tmp_file_nm = self._generate_random_file_name()
         dump(obj, random_tmp_file_nm)
-        self.upload_file(local_file_path=random_tmp_file_nm, s3_key=path_on_s3)
+        self.upload_file(local_file_path=random_tmp_file_nm, s3_key=s3_key)
+
+    def _generate_random_file_name(self):
+        random_tmp_file_nm = "/tmp/tmp_file" + str(uuid.uuid4())
+        return random_tmp_file_nm
 
     def create_bucket(self):
         """
@@ -76,3 +80,26 @@ class S3Util:
         Returns: None
         """
         self.conn.resource(self.boto_type).create_bucket(Bucket=self.bucket)
+
+    def upload_df_parquet(self, df, s3_key):
+        """
+        Exports a datafame to a parquet file on s3
+        Args:
+            df (DataFrame): dataframe to export
+            s3_key (str): The absolute path on s3 to upload the file to
+        Returns: None
+        """
+        random_tmp_file_nm = self._generate_random_file_name()
+        df.to_parquet(random_tmp_file_nm)
+        self.upload_file(random_tmp_file_nm, s3_key)
+
+    def download_df_parquet(self, s3_key, engine='auto', columns=None, **kwargs):
+        """
+        Exports a datafame to a parquet file on s3
+        Args:
+            s3_key (str): The absolute path on s3 to upload the file to
+        Returns: DataFrame
+        """
+        random_tmp_file_nm = self._generate_random_file_name()
+        self.download_file(random_tmp_file_nm, s3_key)
+        return pd.read_parquet(random_tmp_file_nm, engine=engine, columns=columns, **kwargs)
--- tests/aws/test_s3.py
+++ tests/aws/test_s3.py
@@ -2,7 +2,9 @@ import os
 import uuid
 from unittest import TestCase
 
+import pandas as pd
 from moto import mock_s3
+from pandas.util.testing import assert_frame_equal
 
 from hip_data_tools.authenticate import AwsConnection
 from hip_data_tools.aws.s3 import S3Util
@@ -43,6 +45,18 @@ class TestS3Util(TestCase):
         s3u.create_bucket()
         upload_key = "temp.pickle"
         test_object = {"this": "is good"}
-        s3u.serialise_and_upload_object(obj=test_object, path_on_s3=upload_key)
+        s3u.serialise_and_upload_object(obj=test_object, s3_key=upload_key)
         actual_object = s3u.download_object_and_deserialse(s3_key=upload_key)
         self.assertEqual(test_object, actual_object)
+
+    @mock_s3
+    def test_should__upload_dataframe_and_download_parquet__when_using_s3util(self):
+        bucket = "TEST_BUCKET3"
+        conn = AwsConnection(mode="standard_env_var", region_name="ap-southeast-2", settings={})
+        s3u = S3Util(conn=conn, bucket=bucket)
+        s3u.create_bucket()
+        upload_key = "temp.pickle"
+        test_object = pd.DataFrame([1, 2, 3, 4], columns=["one"])
+        s3u.upload_df_parquet(df=test_object, s3_key=upload_key)
+        redown_df = s3u.download_df_parquet(upload_key)
+        assert_frame_equal(test_object, redown_df)
