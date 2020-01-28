9aceea12621ab23f91e76999288411d052633f88
Arihant Surana <ari.surana@gmail.com>
2020-01-29 09:21:44 +1100

    fix unittests

--- hip_data_tools/etl/s3_to_cassandra.py
+++ hip_data_tools/etl/s3_to_cassandra.py
@@ -10,8 +10,10 @@ from hip_data_tools.apache.cassandra import CassandraUtil, CassandraConnectionMa
 from hip_data_tools.aws.common import AwsConnectionSettings, AwsConnectionManager
 from hip_data_tools.aws.s3 import S3Util
 
+
 @dataclass
 class S3ToCassandraSettings:
+    """S3 to Cassandra ETL settings"""
     source_bucket: str
     source_key_prefix: str
     source_connection_settings: AwsConnectionSettings
@@ -23,6 +25,12 @@ class S3ToCassandraSettings:
 
 
 class S3ToCassandra:
+    """
+    Class to transfer parquet data from s3 to Cassandra
+    Args:
+        settings (S3ToCassandraSettings): the settings around the etl to be executed
+    """
+
     def __init__(self, settings: S3ToCassandraSettings):
         self.settings = settings
         self._cassandra = CassandraUtil(
@@ -36,8 +44,11 @@ class S3ToCassandra:
         self.keys_to_transfer = None
 
     def create_table(self):
-
-        df = self._s3.download_df_parquet(s3_key=self.list_source_files()[0])
+        """
+        Creates the destination cassandra table if not exists
+        Returns: None
+        """
+        df = self._s3.download_df_parquet(s3_key=self._list_source_files()[0])
         self._cassandra.create_table_from_dataframe(
             data_frame=df,
             table_name=self.settings.destination_table,
@@ -46,19 +57,30 @@ class S3ToCassandra:
         )
 
     def create_and_upsert_all(self):
+        """
+        First creates the table and then upserts all s3 files to the table
+        Returns: None
+        """
         self.create_table()
-        for key in self.list_source_files():
-            self.upsert_object(key)
+        self.upsert_all()
+
+    def upsert_all(self):
+        """
+        Upsert all files from s3 sequentially into cassandra
+        Returns: None
+        """
+        for key in self._list_source_files():
+            self._upsert_object(key)
             result = self._cassandra.read_dict(f"""
             SELECT COUNT(1) 
             FROM {self.settings.destination_keyspace}.{self.settings.destination_table}""")
             log.info("The target table now contains %s rows", result)
 
-    def upsert_object(self, key):
+    def _upsert_object(self, key):
         df = self._s3.download_df_parquet(s3_key=key)
         self._cassandra.upsert_dataframe(dataframe=df, table=self.settings.destination_table)
 
-    def list_source_files(self):
+    def _list_source_files(self):
         if self.keys_to_transfer is None:
             self.keys_to_transfer = self._s3.list_objects(self.settings.source_key_prefix)
             log.info("Listed and cached %s source files", len(self.keys_to_transfer))
