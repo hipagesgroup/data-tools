21f64a4647d89193e3f3f97aa8e59a3982bae68d
vidura <viduramudalige@hip01186.corp.hipagesgroup.com.au>
2020-02-06 17:51:34 +1100

    feat: [DP-254] Create utility to read from Google Sheets in gandalf

--- tests/google/test_sheets.py
+++ tests/google/test_sheets.py
@@ -11,17 +11,18 @@ class TestS3Util(TestCase):
                  'https://www.googleapis.com/auth/drive']
 
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            '../resources/hip-gandalf-sheets-5c4f5483f529.json', scope)
-        cls.sheet_util = SheetUtil(credentials=credentials)
+            '../resources/key-file.json', scope)
+        cls.sheet_util = SheetUtil(credentials=credentials, database='test', connection=None)
 
     @classmethod
     def tearDownClass(cls):
         return
 
     def test_should__return_the_values_in_a_given_google_sheet__when_using_sheetUtil(self):
+        self.skipTest("This test needs a key file")
         workbook_name = 'Tradie Acquisition Targets'
         sheet_name = 'Sheet1'
-        actual = self.sheet_util.get_all_values(workbook_name, sheet_name)
+        actual = self.sheet_util._get_value_matrix(workbook_name, sheet_name)
         expected = [['Jan-18', 'Feb-18', 'Mar-18', 'Apr-18', 'May-18', 'Jun-18', 'Jul-18', 'Aug-18', 'Sep-18', 'Oct-18',
                      'Nov-18', 'Dec-18', 'Jan-19', 'Feb-19', 'Mar-19', 'Apr-19', 'May-19', 'Jun-19', 'Jul-19', 'Aug-19',
                      'Sep-19', 'Oct-19', 'Nov-19', 'Dec-19', 'Jan-20', 'Feb-20', 'Mar-20', 'Apr-20', 'May-20',
@@ -29,7 +30,6 @@ class TestS3Util(TestCase):
                     ['4,092', '3,192', '3,192', '2,800', '3,015', '3,015', '3,100', '3,415', '3,600', '3,570', '3,210',
                      '1,900', '3,100', '2,747', '2,631', '2,419', '2,769', '3,163', '2,792', '3,018', '2,920', '3,541',
                      '3,128', '2,020', '3,678', '3,522', '3,534', '3,078', '3,114', '3,206']]
-        print(actual)
         self.assertEqual(actual, expected)
 
     def test_should__return_the_table_settings__when_using_sheetUtil(self):
@@ -40,16 +40,13 @@ class TestS3Util(TestCase):
                        'Aug-19',
                        'Sep-19', 'Oct-19', 'Nov-19', 'Dec-19', 'Jan-20', 'Feb-20', 'Mar-20', 'Apr-20', 'May-20',
                        'Jun-20']
-        partitions = ['sheet']
         s3_bucket = "test"
         s3_dir = "abc"
-        actual = self.sheet_util.get_table_settings(table_name, field_names, partitions, s3_bucket, s3_dir)
+        actual = self.sheet_util._get_table_settings(table_name, field_names, s3_bucket, s3_dir)
         expected = {
             "table": "abc",
             "exists": True,
-            "partitions": [
-                {"column": "sheet", "type": "string"}
-            ],
+            "partitions": [],
             "columns": [
                 {'column': 'Jan-18', 'type': 'string'},
                 {'column': 'Feb-18', 'type': 'string'},
@@ -87,5 +84,19 @@ class TestS3Util(TestCase):
             "s3_dir": "abc",
             "encryption": False
         }
-        print(actual)
         self.assertEqual(actual, expected)
+
+    def test_should__return_the_insert_query__when_using_sheetUtil(self):
+        table_name = "test_table"
+        values_matix = [
+            ['4,092', '3,192', '3,192', '2,800', '3,015', '3,015', '3,100', '3,415', '3,600', '3,570', '3,210',
+             '1,900', '3,100', '2,747', '2,631', '2,419', '2,769', '3,163', '2,792', '3,018', '2,920', '3,541',
+             '3,128', '2,020', '3,678', '3,522', '3,534', '3,078', '3,114', '3,206'],
+            ['6,343', '4,192', '9,192', '2,800', '3,015', '3,015', '3,100', '3,415', '3,600', '3,570', '3,210',
+             '1,900', '3,100', '2,747', '2,631', '2,419', '2,769', '3,163', '2,792', '3,018', '2,920', '3,541',
+             '3,128', '2,020', '3,678', '3,522', '3,534', '3,078', '3,114', '8,206']]
+        expected = """
+            INSERT INTO test_table VALUES ('4,092', '3,192', '3,192', '2,800', '3,015', '3,015', '3,100', '3,415', '3,600', '3,570', '3,210', '1,900', '3,100', '2,747', '2,631', '2,419', '2,769', '3,163', '2,792', '3,018', '2,920', '3,541', '3,128', '2,020', '3,678', '3,522', '3,534', '3,078', '3,114', '3,206'), ('6,343', '4,192', '9,192', '2,800', '3,015', '3,015', '3,100', '3,415', '3,600', '3,570', '3,210', '1,900', '3,100', '2,747', '2,631', '2,419', '2,769', '3,163', '2,792', '3,018', '2,920', '3,541', '3,128', '2,020', '3,678', '3,522', '3,534', '3,078', '3,114', '8,206')
+        """
+        actual = self.sheet_util._get_the_insert_query(table_name, values_matix)
+        self.assertEqual(actual.strip(), expected.strip())
