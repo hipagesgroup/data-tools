f16a9b83aed59306126c6834f0a294c82875b695
vidura <viduramudalige@hip01186.corp.hipagesgroup.com.au>
2020-02-06 17:51:34 +1100

    feat: [DP-254] Create utility to read from Google Sheets in gandalf

--- hip_data_tools/google/sheets.py
+++ hip_data_tools/google/sheets.py
@@ -1,20 +1,15 @@
 import gspread
 
+from hip_data_tools.aws.athena import AthenaUtil
+
 
 class SheetUtil:
 
-    def __init__(self, credentials):
+    def __init__(self, credentials, database, connection):
         self.gc = gspread.authorize(credentials)
+        self.au = AthenaUtil(database=database, conn=connection)
 
-    def get_all_values(self, workbook_name, sheet_name, cell_range='', skip_top_rows_count=0):
-        """
-
-        :param skip_top_rows_count:
-        :param cell_range:
-        :param sheet_name:
-        :param workbook_name:
-        :return:
-        """
+    def _get_value_matrix(self, workbook_name, sheet_name, cell_range='', skip_top_rows_count=0):
         worksheet = self.gc.open(workbook_name).worksheet(sheet_name)
         list_of_lists = []
 
@@ -27,7 +22,7 @@ class SheetUtil:
                 list_of_lists.append(cell.value)
             return list_of_lists
 
-    def get_table_settings(self, table_name, field_names, partitions, s3_bucket, s3_dir):
+    def _get_table_settings(self, table_name, field_names, s3_bucket, s3_dir):
         table_settings = {
             "table": table_name,
             "exists": True,
@@ -43,12 +38,36 @@ class SheetUtil:
             columns.append({"column": field_name, "type": "string"})
         table_settings["columns"] = columns
 
-        partition_settings = []
-        for partition_name in partitions:
-            partition_settings.append({"column": partition_name, "type": "string"})
-        table_settings["partitions"] = partition_settings
-
         return table_settings
 
-    def get_the_update_query(self, values_matrix, field_names):
-        return
+    def _get_the_insert_query(self, table_name, values_matrix):
+        if not values_matrix:
+            return "INSERT INTO {table_name} VALUES ()".format(table_name=table_name)
+        insert_query = "INSERT INTO {table_name} VALUES ".format(table_name=table_name)
+        values = ""
+        for value in values_matrix:
+            values += "({}), ".format(', '.join(["'{}'".format(val) for val in value]))
+        values = values[:-2]
+        insert_query += values
+        return insert_query
+
+    def load_sheet_to_athena(self, workbook_name, sheet_name, table_name, field_names, s3_bucket, s3_dir,
+                             skip_top_rows_count=0):
+        """
+
+        :param workbook_name:
+        :param sheet_name:
+        :param table_name:
+        :param field_names:
+        :param s3_bucket:
+        :param s3_dir:
+        :param skip_top_rows_count:
+        :return:
+        """
+        values_matrix = self._get_value_matrix(workbook_name=workbook_name, sheet_name=sheet_name,
+                                               skip_top_rows_count=skip_top_rows_count)
+        table_settings = self._get_table_settings(table_name=table_name, field_names=field_names, s3_bucket=s3_bucket,
+                                                  s3_dir=s3_dir)
+        self.au.create_table(table_settings)
+        insert_query = self._get_the_insert_query(table_name=table_name, values_matrix=values_matrix)
+        self.au.run_query(query_string=insert_query)
