b060cdf389850733da510890ad805137515c7d21
Arihant Surana <ari.surana@gmail.com>
2020-02-10 17:07:06 +1100

    Update hip_data_tools/aws/s3.py
    Co-Authored-By: Fabian <fabian.buske@gmail.com>

--- hip_data_tools/aws/s3.py
+++ hip_data_tools/aws/s3.py
@@ -335,7 +335,7 @@ class S3Util(AwsUtil):
         """
         Upload a binary stream of data as an s3 object's body
         Args:
-            stream: the stream of bytes tobe uploaded
+            stream: the stream of bytes to be uploaded
             key: s3 key at which thios stream is to be uploaded
         Returns: None
         """
