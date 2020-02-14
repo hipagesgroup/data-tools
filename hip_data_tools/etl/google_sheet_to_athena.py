0223df75fac7a0661c75ef60e9a6a5eb2d35469c
vidura <viduramudalige@hip01186.corp.hipagesgroup.com.au>
2020-02-14 17:19:14 +1100

    Added dynamic field type support

--- hip_data_tools/etl/google_sheet_to_athena.py
+++ hip_data_tools/etl/google_sheet_to_athena.py
@@ -2,6 +2,7 @@
 Module to deal with data transfer from Google sheets to Athena
 """
 import logging as log
+import re
 
 from attr import dataclass
 
@@ -13,15 +14,41 @@ from hip_data_tools.google.common import GoogleApiConnectionSettings
 from hip_data_tools.google.sheets.common import GoogleSheetConnectionManager
 from hip_data_tools.google.sheets.sheets import SheetUtil
 
+DTYPE_GOOGLE_SHEET_TO_PARQUET_ATHENA = {
+    "NUMBER": "DOUBLE",
+    "STRING": "STRING",
+    "BOOLEAN": "BOOLEAN"
+}
+
 
 @dataclass
 class GoogleSheetsToAthenaSettings:
-    """Google sheets to Athena ETL settings"""
+    """
+    Google sheets to Athena ETL settings
+    Args:
+        workbook_name: the name of the workbook (eg: Tradie Acquisition Targets)
+        sheet_name: name of the google sheet (eg: sheet1)
+        row_range: range of rows (eg: '2:5')
+        table_name: name of the athena table (eg: 'sheet_table')
+        fields: list of sheet field names and types. Field names cannot contain hyphens('-')
+            (eg: ['name:string','age:number','is_member:boolean'])
+        use_derived_types: if this is false type of the fields are considered as strings irrespective of the provided
+             field types (eg: True)
+        s3_bucket: s3 bucket to store the files (eg: au-test-bucket)
+        s3_dir: s3 directory to store the files (eg: sheets/new)
+        skip_top_rows_count: number of top rows that need to be skipped (eg: 1)
+        key_file_path: path of the google api key file (eg: path/key_file.json)
+        database: name of the athena database (eg: dev)
+        region: aws service region (eg: ap-southeast-2)
+        profile: aws credentials profile (eg: default)
+        secrets_manager: aws secret manager object
+    """
     workbook_name: str
     sheet_name: str
-    cell_range: str
+    row_range: str
     table_name: str
-    field_names: list
+    fields: list
+    use_derived_types: bool
     s3_bucket: str
     s3_dir: str
     skip_top_rows_count: int
@@ -58,12 +85,21 @@ class GoogleSheetToAthena:
                 AwsConnectionSettings(region=self.settings.region, secrets_manager=self.settings.secrets_manager,
                                       profile=self.settings.profile)))
 
-    def _get_table_settings(self, table_name, field_names, s3_bucket, s3_dir):
+    def _simplified_dtype(self, data_type):
+        """
+        Return the athena base data type
+        Args:
+            data_type (string): data type
+        :return:
+        """
+        return ((re.sub(r'\(.*\)', '', data_type)).split(" ", 1)[0]).upper()
+
+    def _get_table_settings(self, table_name, fields, s3_bucket, s3_dir):
         """
         Get the table settings dictionary
         Args:
             table_name (string): name of the athena table
-            field_names (list): list of field names
+            fields (list): list of field names and types (eg: ['name:string','age:number','is_member:boolean'])
             s3_bucket (string): s3 bucket name
             s3_dir (string): s3 directory
         Returns: table settings dictionary
@@ -80,8 +116,19 @@ class GoogleSheetToAthena:
             "encryption": False
         }
         columns = []
-        for field_name in field_names:
-            columns.append({"column": field_name, "type": "string"})
+        if self.settings.use_derived_types:
+            for field in fields:
+                field_name_type = field.split(':')
+                field_name = field_name_type[0]
+                field_type = field_name_type[1]
+                columns.append({"column": field_name,
+                                "type": DTYPE_GOOGLE_SHEET_TO_PARQUET_ATHENA.get(
+                                    str(self._simplified_dtype(field_type)),
+                                    "STRING")})
+        else:
+            for field in fields:
+                field_name = field.split(':')[0]
+                columns.append({"column": field_name, "type": "string"})
         table_settings["columns"] = columns
 
         return table_settings
@@ -120,11 +167,11 @@ class GoogleSheetToAthena:
             s3_util.delete_recursive(self.settings.s3_dir)
         values_matrix = sheet_util.get_value_matrix(workbook_name=self.settings.workbook_name,
                                                     sheet_name=self.settings.sheet_name,
-                                                    row_range=self.settings.cell_range,
+                                                    row_range=self.settings.row_range,
                                                     skip_top_rows_count=self.settings.skip_top_rows_count)
         log.info("The value matrix:\n %s", values_matrix)
         table_settings = self._get_table_settings(table_name=self.settings.table_name,
-                                                  field_names=self.settings.field_names,
+                                                  fields=self.settings.fields,
                                                   s3_bucket=self.settings.s3_bucket,
                                                   s3_dir=self.settings.s3_dir)
         athena_util.create_table(table_settings)
