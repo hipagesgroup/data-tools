e4f04d9b9e4dbb8c7888103df563ae75aa7882d7
Arihant Surana <ari.surana@gmail.com>
2020-03-13 12:50:41 +1100

    Add a secrets file

--- hip_data_tools/google/adwords.py
+++ hip_data_tools/google/adwords.py
@@ -1,7 +1,3 @@
-import json
-import os
-from tempfile import NamedTemporaryFile
-
 from attr import dataclass
 from googleads import oauth2, adwords
 
@@ -12,27 +8,19 @@ class GoogleAdWordsSecretsManager(SecretsManager):
 
     def __init__(self,
                  source: KeyValueSource = ENVIRONMENT,
-                 key_json_var: str = "adwords_key_json",
-                 adwords_developer_token_var="adwords_developer_token"):
-        self._required_keys = [key_json_var, adwords_developer_token_var, ]
+                 client_secret_var: str = "adwords_client_secret",
+                 refresh_token_var: str = "adwords_refresh_token",
+                 developer_token_var="adwords_developer_token"):
+        self._required_keys = [client_secret_var, refresh_token_var, developer_token_var, ]
         super().__init__(self._required_keys, source)
-        self._keyfile_path = None
-        self._key_object = self.get_secret(key_json_var)
-        self.developer_key = self.get_secret(adwords_developer_token_var)
+        self.client_secret = self.get_secret(client_secret_var)
+        self.refresh_token = self.get_secret(refresh_token_var)
+        self.developer_key = self.get_secret(developer_token_var)
 
-    def get_keyfile_path(self):
-        if self._keyfile_path is None:
-            with NamedTemporaryFile(mode="w+", delete=False) as temp_file:
-                json.dump(self._key_object, temp_file)
-                self._keyfile_path = temp_file.name
-        return self._keyfile_path
-
-    def cleanup_keyfile(self):
-        os.remove(self._keyfile_path) if self._keyfile_path else None
 
 @dataclass
 class GoogleAdWordsConnectionSettings:
-    service_account_user: str
+    client_id: str
     user_agent: str
     client_customer_id: str
     secrets_manager: GoogleAdWordsSecretsManager
@@ -42,19 +30,21 @@ class GoogleAdWordsConnectionManager:
     def __init__(self, settings: GoogleAdWordsConnectionSettings):
         self.settings = settings
 
-    def get_client(self):
-        oauth2_client = oauth2.GoogleServiceAccountClient(
-            key_file=self.settings.secrets_manager.get_keyfile_path(),
-            scope=oauth2.GetAPIScope('adwords'),
-            sub=self.settings.service_account_user,  # Check if you need this
-        )
+    def get_adwords_client(self):
+        oauth2_client = self.get_oauth_client()
         adwords_client = adwords.AdWordsClient(
             developer_token=self.settings.secrets_manager.developer_key,
             oauth2_client=oauth2_client,
-            user_agent=self.settings.user_agent,
-            client_customer_id=self.settings.client_customer_id)
-        self.settings.secrets_manager.cleanup_keyfile()
+            user_agent=self.settings.user_agent)
         return adwords_client
+
+    def get_oauth_client(self):
+        oauth2_client = oauth2.GoogleRefreshTokenClient(
+            client_id=self.settings.client_id,
+            client_secret=self.settings.secrets_manager.client_secret,
+            refresh_token=self.settings.secrets_manager.refresh_token
+        )
+        return oauth2_client
 
 
 class AdWordsUtil():
@@ -64,16 +54,11 @@ class AdWordsUtil():
 
     def _get_client(self):
         if self._client is None:
-            self._client = self.conn.get_client()
+            self._client = self.conn.get_adwords_client()
         return self._client
 
     def get_customer_details(self):
-        customer_service = self._get_client().GetService('CustomerService', version='v201702')
+        client = self._get_client()
+        customer_service = client.GetService('CustomerService', version='v201809')
         customers = customer_service.getCustomers()
-        print('You are logged in as a user with access to the following customers:')
-        for customer in customers:
-            print('\t%s' % customer['customerId'])
         return customers
-
-
-
--- tests/google/test_adwords.py
+++ tests/google/test_adwords.py
@@ -1,37 +1,25 @@
+import os
 from unittest import TestCase
+
+from py.builtin import execfile
 
 from hip_data_tools.common import DictKeyValueSource
 from hip_data_tools.google.adwords import AdWordsUtil, GoogleAdWordsConnectionManager, \
     GoogleAdWordsConnectionSettings, GoogleAdWordsSecretsManager
 
 
-class TestS3Util(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        return
-
-    @classmethod
-    def tearDownClass(cls):
-        return
+class TestAdWordsUtil(TestCase):
 
     def test_stuff_works(self):
+        # Load secrets via env vars
+        execfile("../../secrets.py")
         adwords_util = AdWordsUtil(
             GoogleAdWordsConnectionManager(
                 GoogleAdWordsConnectionSettings(
-                    service_account_user="",
+                    client_id=os.getenv("adwords_client_id"),
                     user_agent="Tester",
-                    client_customer_id="",
-                    secrets_manager=GoogleAdWordsSecretsManager(
-                        source=DictKeyValueSource({
-                            "key_json": {},
-                            "adwords_developer_token": ""
-                        }),
-                        key_json_var="key_json",
-                        adwords_developer_token_var="adwords_developer_token"
-                    )
-                )
-            )
-        )
-        expected = None
-        cust = adwords_util.get_customer_details()
-        self.assertEqual(cust, expected)
+                    client_customer_id="840-258-1664",
+                    secrets_manager=GoogleAdWordsSecretsManager())))
+
+        expected = 3
+        self.assertEqual(len(adwords_util.get_customer_details()), expected)
