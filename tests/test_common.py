0b18d0724766fb209003de69b3f87e93e6acec22
Arihant Surana <ari.surana@gmail.com>
2020-04-16 10:13:45 +1000

    feat: [DP-372] remove dependecy on common package for setup.py

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,27 +1,26 @@
-import os
 from unittest import TestCase
 
-from hip_data_tools.common import get_release_version, get_long_description, flatten_nested_dict, \
+from hip_data_tools.common import flatten_nested_dict, \
     to_snake_case, nested_list_of_dict_to_dataframe
 
 
 class TestCommon(TestCase):
-    def test__get_release_version__parses_tag(self):
-        os.environ["GIT_TAG"] = "v1.3"
-        actual = get_release_version()
-        expected = "1.3"
-        self.assertEqual(actual, expected)
-
-    def test__get_release_version__works_without_tag(self):
-        del os.environ["GIT_TAG"]
-        actual = get_release_version()
-        expected = "0.0"
-        self.assertEqual(actual, expected)
-
-    def test__get_long_description__reads_readme(self):
-        actual = get_long_description()[0:16]
-        expected = "# hip-data-tools"
-        self.assertEqual(actual, expected)
+    # def test__get_release_version__parses_tag(self):
+    #     os.environ["GIT_TAG"] = "v1.3"
+    #     actual = get_release_version()
+    #     expected = "1.3"
+    #     self.assertEqual(actual, expected)
+    #
+    # def test__get_release_version__works_without_tag(self):
+    #     del os.environ["GIT_TAG"]
+    #     actual = get_release_version()
+    #     expected = "0.0"
+    #     self.assertEqual(actual, expected)
+    #
+    # def test__get_long_description__reads_readme(self):
+    #     actual = get_long_description()[0:16]
+    #     expected = "# hip-data-tools"
+    #     self.assertEqual(actual, expected)
 
     def test__should__flatten_dict__with_one_level_of_nesting(self):
         input = {
