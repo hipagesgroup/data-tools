46e07452752eff453c932c850b8a7c5a4ac5942e
vidura <viduramudalige@192-168-1-102.tpgi.com.au>
2020-05-21 15:43:47 +1000

    Added a unit test

--- tests/etl/adwords_to_s3.py
+++ tests/etl/adwords_to_s3.py
@@ -44,9 +44,9 @@ class TestAdwordsToS3(TestCase):
 
         actual_payloads = etl.get_parallel_payloads(page_size=1000, number_of_workers=3)
         expected_payloads = [
-            {'worker': 0, 'start_index': 0, 'number_of_pages': 0, 'page_size': 1000},
-            {'worker': 1, 'start_index': 0, 'number_of_pages': 0, 'page_size': 1000},
-            {'worker': 2, 'start_index': 0, 'number_of_pages': 0, 'page_size': 1000}]
+            {'worker': 0, 'start_index': 0, 'number_of_pages': 435, 'page_size': 1000},
+            {'worker': 1, 'start_index': 435000, 'number_of_pages': 435, 'page_size': 1000},
+            {'worker': 2, 'start_index': 870000, 'number_of_pages': 435, 'page_size': 1000}]
 
         self.assertListEqual(expected_payloads, actual_payloads)
 
@@ -59,7 +59,9 @@ class TestAdwordsToS3(TestCase):
             profile=None)
         target_bucket = os.getenv('S3_TEST_BUCKET')
         target_key_prefix = "tmp/test/hip_data_tools/adwords_to_s3/test"
-
+        conn = AwsConnectionManager(aws_setting)
+        s3u = S3Util(conn=conn, bucket=target_bucket)
+        s3u.delete_recursive(target_key_prefix)
         adwords_settings = GoogleAdWordsConnectionSettings(
             client_id=os.getenv("adwords_client_id"),
             user_agent="Tester",
@@ -77,14 +79,52 @@ class TestAdwordsToS3(TestCase):
             target_connection_settings=aws_setting
         )
         etl = AdWordsToS3(etl_settings)
+        etl.build_query(start_index=0, page_size=5, num_iterations=2)
+
+        etl.transfer_all()
+
+        actual = s3u.get_keys(target_key_prefix)
+        print(actual)
+        expected = ['tmp/test/hip_data_tools/adwords_to_s3/test/index_0__4.parquet',
+                    'tmp/test/hip_data_tools/adwords_to_s3/test/index_5__9.parquet']
+        self.assertListEqual(expected, actual)
+
+    def test__should__add_file_name_prefix__when__file_name_prefix_is_provided(self):
+        # Load secrets via env vars
+        execfile("../../secrets.py")
+        aws_setting = AwsConnectionSettings(
+            region="ap-southeast-2",
+            secrets_manager=AwsSecretsManager(),
+            profile=None)
+        target_bucket = os.getenv('S3_TEST_BUCKET')
+        target_key_prefix = "tmp/test/hip_data_tools/adwords_to_s3/test"
+        conn = AwsConnectionManager(aws_setting)
+        s3u = S3Util(conn=conn, bucket=target_bucket)
+        s3u.delete_recursive(target_key_prefix)
+        adwords_settings = GoogleAdWordsConnectionSettings(
+            client_id=os.getenv("adwords_client_id"),
+            user_agent="Tester",
+            client_customer_id=os.getenv("adwords_client_customer_id"),
+            secrets_manager=GoogleAdWordsSecretsManager())
+
+        etl_settings = AdWordsToS3Settings(
+            source_query_fragment=ServiceQueryBuilder().Select('Id').OrderBy('Id'),
+            source_service="AdGroupAdService",
+            source_service_version="v201809",
+            source_connection_settings=adwords_settings,
+            target_bucket=target_bucket,
+            target_key_prefix=target_key_prefix,
+            target_file_prefix="12345678",
+            target_connection_settings=aws_setting
+        )
+        etl = AdWordsToS3(etl_settings)
         etl.build_query(start_index=786000, page_size=5, num_iterations=2)
 
         etl.transfer_all()
-        conn = AwsConnectionManager(aws_setting)
-        s3u = S3Util(conn=conn, bucket=target_bucket)
+
         actual = s3u.get_keys(target_key_prefix)
-        print(actual)
-        expected = ['tmp/test/hip_data_tools/adwords_to_s3/test/348937894index_0__99.parquet',
-                    'tmp/test/hip_data_tools/adwords_to_s3/test/index_786000__786004.parquet',
-                    'tmp/test/hip_data_tools/adwords_to_s3/test/index_786005__786009.parquet']
+        expected = [
+            'tmp/test/hip_data_tools/adwords_to_s3/test/12345678index_786000__786004.parquet',
+            'tmp/test/hip_data_tools/adwords_to_s3/test/12345678index_786005__786009.parquet']
+
         self.assertListEqual(expected, actual)
