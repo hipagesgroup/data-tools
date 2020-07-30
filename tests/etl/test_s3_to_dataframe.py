5777667b9b81285f2565f453f8d29ac7779599dc
vbmudalige <vbmudalige@gmail.com>
2020-07-30 19:48:21 +1000

    Fixed unit tests

--- tests/etl/test_s3_to_dataframe.py
+++ tests/etl/test_s3_to_dataframe.py
@@ -74,8 +74,7 @@ class TestS3ToDataFrame(TestCase):
 
         expected = pd.DataFrame(dict(A=range(20)),
                                 index=pd.date_range('20130101', periods=20, freq='d'))
-        print(expected.to_dict())
-        print(etl.get_all_files_as_data_frame().to_dict())
+
         assert_frame_equal(expected, etl.get_all_files_as_data_frame(), check_like=True)
 
     @mock_s3
@@ -108,6 +107,6 @@ class TestS3ToDataFrame(TestCase):
 
         etl = S3ToDataFrame(settings)
 
-        assert_frame_equal(df1, etl.next())
-        assert_frame_equal(df2, etl.next())
-        assert_frame_equal(df3, etl.next())
+        assert_frame_equal(df1, etl.next(), check_like=True)
+        assert_frame_equal(df2, etl.next(), check_like=True)
+        assert_frame_equal(df3, etl.next(), check_like=True)
