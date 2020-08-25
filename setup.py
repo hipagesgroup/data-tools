69a8edd332a2812628fc2b7c9c373eabe8f9aa83
vbmudalige <vbmudalige@gmail.com>
2020-08-25 15:18:08 +1000

    Pinned boto3

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     install_requires=[
         "stringcase",
         "attrs",
-        "boto3==1.14.46",
+        "boto3==1.14.45",
         "joblib",
         "pandas",
         "GitPython",
