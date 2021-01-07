085a49438aec5ab1e27949f4195965d8db81f2f2
Fabian <fabian.buske@gmail.com>
2021-01-07 19:52:17 +1100

    feat: pass parameters to parquet writer (#132)
    * pass parameters to parquet writer

--- tests/aws/test_s3.py
+++ tests/aws/test_s3.py
@@ -119,7 +119,7 @@ class TestS3Util(TestCase):
         self.s3.create_bucket()
         upload_key = "temp456"
         test_object = pd.DataFrame([1, 2, 3, 4], columns=["one"])
-        self.s3.upload_dataframe_as_parquet(dataframe=test_object, key=upload_key)
+        self.s3.upload_dataframe_as_parquet(dataframe=test_object, key=upload_key, use_deprecated_int96_timestamps=True)
         redown_df = self.s3.download_parquet_as_dataframe(f"{upload_key}/data.parquet")
         assert_frame_equal(test_object, redown_df)
 
