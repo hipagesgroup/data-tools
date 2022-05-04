be5e709121d59a8a20b02de23df8bf587fab9b62
Shubhank Vijayvergiya <39094864+shubhank-v@users.noreply.github.com>
2022-05-05 00:04:30 +1000

    Revert "chore: Set debug (#157)"
    This reverts commit aa5124927d24582c3eb916883cf005a0d8c52c1b.

--- hip_data_tools/etl/athena_to_googleads.py
+++ hip_data_tools/etl/athena_to_googleads.py
@@ -230,7 +230,7 @@ class AthenaToGoogleAdsOfflineConversion(AthenaToDataFrame):
         for dat in success:
             self._get_sink_manager(dat).succeeded()
         for dat in fail:
-            self._get_sink_manager(dat).failed()
+            self._get_sink_manager(dat["data"]).failed()
 
     def _verify_data_before_upsert(self, data: List[dict]) -> (List[dict], List[dict]):
         data, issues = map(list, zip(*[self._sanitise_data(dat) for dat in data]))
